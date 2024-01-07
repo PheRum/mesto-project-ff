aafe453ce1ce3a613e8a9f05fd44b03b2578d4f9
PheRum <pherum83@gmail.com>
2024-01-07 16:40:31 +0300

    #2 - webpack (#2)

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Проектная работа Mesto
+# Проектная работа Место
 
 https://github.com/PheRum/mesto-project-ff
 
 ```bash
 git clone https://github.com/PheRum/mesto-project-ff
+
+yarn install
+yarn dev
 ```
---
+++ src/components/card.js
@@ -0,0 +1,50 @@
+import { checkImageLink } from '@/components/util';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placeContainer = document.querySelector('.places__list');
+
+export async function createCard(data, removeCard, likeCard, handleCardImageClick) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const link = await checkImageLink(data.link);
+
+    card.querySelector('.card__image').src = link;
+    card.querySelector('.card__image').alt = data.name;
+    card.querySelector('.card__title').textContent = data.name;
+
+    card.querySelector('.card__image').addEventListener('click', () => {
+        handleCardImageClick({
+            name: data.name,
+            link: link,
+        });
+    });
+
+    card.querySelector('.card__like-button').addEventListener('click', likeCard);
+    card.querySelector('.card__delete-button').addEventListener('click', removeCard);
+
+    return card;
+}
+
+export function likeCard(e) {
+    const btn = e.target;
+
+    btn.classList.toggle('card__like-button_is-active');
+}
+
+export function removeCard(e) {
+    const cardContainer = e.target.closest('li');
+
+    cardContainer.remove();
+}
+
+export function renderCard(card, type = 'append') {
+    switch (type) {
+        case 'append':
+            placeContainer.append(card);
+            break;
+
+        case 'prepend':
+        default:
+            placeContainer.prepend(card);
+            break;
+    }
+}
